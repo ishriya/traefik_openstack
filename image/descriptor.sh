b2c52fdb797b75655c05b92dcba89892d2e3cbd3
Ishwariya Raveendran <48996604+ishriya@users.noreply.github.com>
2020-03-03 22:09:47 +0100

    Create descriptor.sh

---
+++ image/descriptor.sh
@@ -0,0 +1,13 @@
+wget http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
+
+virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
+      --run-command 'echo "manage_etc_hosts: true" >> /etc/cloud/cloud.cfg' \
+      --mkdir /tngbench_share \
+      --upload log_intf_statistics.py:/log_intf_statistics.py \
+      --upload start.sh:/start.sh \
+      --upload stop.sh:/stop.sh \
+      --run-command 'chmod +x start.sh' \
+      --run-command 'chmod +x stop.sh' \
+      --upload install.sh:/install.sh \
+      --run install.sh \
+      --upload traefik.toml:/etc/traefik/traefik.toml
